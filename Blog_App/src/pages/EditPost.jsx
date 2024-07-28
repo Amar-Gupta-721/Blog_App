24708f691c0c5a3a2f2cc74200683609150f660d
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-07-28 13:12:36 +0530

    testing the app

---
+++ Blog_App/src/pages/EditPost.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import { Container, PostForm } from '../components'
+import appwritreService from '../appwrite/config'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function EditPost() {
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()      // URL se value nikalenge
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug){
+            appwritreService.getPost(slug).then((post)=>{
+                if(post){
+                    setPost(post)
+                }
+            })
+        }
+        else{
+            navigate('/')
+        }
+    },[slug,navigate])
+  return post ? (
+    <div className="py-8">
+        <Container>
+            <PostForm post={post}/>
+        </Container>
+    </div>
+  ) : null
+}
+
+export default EditPost
