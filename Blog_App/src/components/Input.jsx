0f4b7d25d986a30b13442771fe3d80e1b829c08c
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-07-29 00:15:35 +0530

    Update Input.jsx

--- Blog_App/src/components/Input.jsx
+++ Blog_App/src/components/Input.jsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef(function Input({
             }
             <input type={type} 
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref={ref}           //yhi wo cheez hai jo ref degi apko apne parent component ke andr
+            ref={ref}      
             {...props}
             id={id}
              />
