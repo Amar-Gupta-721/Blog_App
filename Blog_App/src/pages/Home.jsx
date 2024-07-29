064b5f050aa501a2a1a299b3fba1368c400e3b01
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-07-29 19:07:12 +0530

    Update Home.jsx

--- Blog_App/src/pages/Home.jsx
+++ Blog_App/src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import appwriteService from '../appwrite/config'
 import { Container, PostCard } from '../components'
+import { useSelector } from 'react-redux'       
+import { useNavigate } from 'react-router-dom'  
 
 function Home() {
+    const userData = useSelector(state=>state.auth.userData)    
+    const navigate = useNavigate()
     const [posts, setPosts] = useState([])
+    
     useEffect(()=>{
-        appwriteService.getPosts().then((posts)=>{
-            if(posts){
-                setPosts(posts.documents)
-            }
-        })
+        if(userData){       
+            appwriteService.getPosts().then((posts)=>{
+                if(posts){
+                    setPosts(posts.documents)
+                }
+            })
+            navigate("/")
+        }
+        else{
+            navigate("/login")      
+        }   
     },[])
 
   if(posts.length === 0){
