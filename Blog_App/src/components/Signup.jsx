e4f1b955a47f969c29c50c1aa737132ea3210aa5
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-07-30 19:05:36 +0530

    Update Signup.jsx

--- Blog_App/src/components/Signup.jsx
+++ Blog_App/src/components/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
             if(userData){
                 const userData = await authService.getCurrentUser()
                 if(userData)dispatch(login(userData))
-                navigate("/")
+                setTimeout(()=>(navigate("/")),0)
             }
         } catch (error) {
             setError(error.message)
@@ -34,15 +34,8 @@ function Signup() {
                 </span>
             </div>
             <h2 className="text-center text-2xl font-bold leading-tight">
-                Sign in to your account </h2>  
-                <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
-                    <Link to="/signup"
-                    className="font-medium text-primary transition-all duration-200 hover:underline"
-                    >
-                        Sign Up
-                    </Link>
-            </p>
+                Create a new Account 
+            </h2>
             {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
             <form onSubmit={handleSubmit(create)}>
@@ -76,7 +69,7 @@ function Signup() {
                     />
                     <Button
                     type="submit"
-                    className="w-full"
+                    className="w-full transition ease-in-out delay-10 bg-blue-500 hover:scale-105 hover:bg-blue-900 duration-200 active:bg-blue-300"
                     children="Create Account"
                     ></Button>
                 </div>
