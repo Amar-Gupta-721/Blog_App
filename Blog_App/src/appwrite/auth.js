78e0bd528f2196585513f61932dcf9ef90cc902b
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-08-05 21:27:49 +0530

    Update auth.js

--- Blog_App/src/appwrite/auth.js
+++ Blog_App/src/appwrite/auth.js
@@ -1,48 +1,60 @@
 import conf from '../conf/conf.js';
-import {Client, Account, ID} from 'appwrite'
-
+import { Client, Account, ID } from 'appwrite';
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
 
-    async createAccount({email, password, name}){
-        try{
-            const userAccount = await this.account.create
-            (ID.unique(), email, password, name);
-            if(userAccount){
-                // call another method
-                return this.login({email, password});
+    async createAccount({ email, password, name }) {
+        try {
+            // Create user account
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // Log in the user
+                await this.login({ email, password });
+
+                // Send verification email
+                await this.sendVerificationEmail();
+                return userAccount;
             } else {
                 return userAccount;
             }
         } catch (error) {
-            console.log("Appwrite service :: createAccount :: error ",error);
+            console.log("Appwrite service :: createAccount :: error ", error);
             throw error;
         }
     }
 
-    async login({email, password}){
-        try{
+    async sendVerificationEmail() {
+        try {
+            return await this.account.createVerification("http://localhost:5173/verify-email");
+        } catch (error) {
+            console.log("Appwrite service :: sendVerificationEmail :: error ", error);
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
             return await this.account.createEmailPasswordSession(email, password);
         } catch (error) {
-            console.log("Appwrite service :: login :: error ",error);
+            console.log("Appwrite service :: login :: error ", error);
             throw error;
         }
     }
 
-    async getCurrentUser(){
+    async getCurrentUser() {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error ",error);
+            console.log("Appwrite service :: getCurrentUser :: error ", error);
             throw error;
         }
     }
@@ -51,7 +63,16 @@ export class AuthService {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: logout :: error ",error);
+            console.log("Appwrite service :: logout :: error ", error);
+            throw error;
+        }
+    }
+
+    async verifyEmail(userId, secret) {
+        try {
+            return await this.account.updateVerification(userId, secret);
+        } catch (error) {
+            console.log("Appwrite service :: verifyEmail :: error ", error);
             throw error;
         }
     }
