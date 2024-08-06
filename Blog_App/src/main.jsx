6d186d3215a8049e11f5e313f583cc82660dba10
Amar-Gupta-721 <158589288+Amar-Gupta-721@users.noreply.github.com>
2024-08-06 23:52:20 +0530

    Update main.jsx

--- Blog_App/src/main.jsx
+++ Blog_App/src/main.jsx
@@ -1,76 +1,77 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { AuthLayout } from './components/index.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { AuthLayout } from './components/index.js';
 
-
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
-import AllPosts from './pages/AllPosts.jsx'
-import AddPost from './pages/AddPost.jsx'
-import EditPost from './pages/EditPost.jsx'
-import Post from './pages/Post.jsx'
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import AllPosts from './pages/AllPosts.jsx';
+import AddPost from './pages/AddPost.jsx';
+import EditPost from './pages/EditPost.jsx';
+import Post from './pages/Post.jsx';
+import EmailVerification from './pages/EmailVerification.jsx';
 
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <App/>,
-    children : [
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path : "/",
-        element : <Home/>
+        path: "/",
+        element: <Home />
       },
       {
-        path : "/login",
-        element : (
+        path: "/login",
+        element: (
           <AuthLayout authentication={false}>
-            <Login/>
+            <Login />
           </AuthLayout>
         )
       },
       {
-        path : "/signup",
-        element : (
+        path: "/signup",
+        element: (
           <AuthLayout authentication={false}>
-            <Signup/>
+            <Signup />
           </AuthLayout>
         )
       },
       {
-        path : "/all-posts",
-        element : (
+        path: "/all-posts",
+        element: (
           <AuthLayout authentication>
-            {" "}
-            <AllPosts/>
+            <AllPosts />
           </AuthLayout>
         )
       },
       {
-        path : "/add-post",
-        element : (
+        path: "/add-post",
+        element: (
           <AuthLayout authentication>
-            {" "}
-            <AddPost/>
+            <AddPost />
           </AuthLayout>
         )
       },
       {
-        path : "/edit-post:slug",
-        element : (
+        path: "/edit-post/:slug",
+        element: (
           <AuthLayout authentication>
-            {" "}
-            <EditPost/>
+            <EditPost />
           </AuthLayout>
         )
       },
       {
-        path : "/post/:slug",
-        element : <Post/>
+        path: "/post/:slug",
+        element: <Post />
+      },
+      {
+        path: "/verify-email",
+        element: <EmailVerification />
       }
     ]
   }
@@ -79,7 +80,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
